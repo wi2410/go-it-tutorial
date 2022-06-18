f9be11c172d2946bbc1a8f9459af2425c764a0ee
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-06-18 17:13:02 +0300

    add files

---
+++ js/tutorial.js
@@ -0,0 +1,35 @@
+// Task 1
+// Напишите 2 цикла:
+// - цикл, который выводит в консоль числа от max до min по убыванию
+// - цикл, который выводит в консоль все четные числа от min до max
+
+// const max = 50;
+// const min = 23;
+// for ( let = max; i>= min; i--) {
+//    console.log(i)
+// }
+// for (let = min; i <= max; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+
+// }
+
+// Task 2
+// Запитайте у користувача якого він року народження. Поточний рік візьміть за константу.
+// Виведіть через alert повідомлення для юзера з інформацією, яка вказує на його вік.
+// Наприклад: "Ваш вік - хх років"
+// Для автоматичного визначення поточного року можете використати слідуючий функціонал:
+// const date = new Date();
+// console.log(date.getFullYear());
+// const yearOfBirth = prompt ("Введіть ваш рік народження");
+// alert(`Ваш вік - ${date.getFullYear() - yearOfBirth} років`);
+
+// Task 3
+// Реалізуйте result
+// const a = ["a", "b", "c", "d", "e", "f", "g", "h"];
+// const firstA = a.slice(0, 3);
+// const secondA = a.slice(-2)
+// const result = firstA.concat(firstA, secondA)
+
+// console.log(result); // ["a", "b", "c", "a", "b", "c", "g", "h"]
