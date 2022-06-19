f5fc16b8cad8caf9d2b8ccc37b5404f96cb0f152
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-06-19 19:35:29 +0300

    add file

---
+++ js/vebinar.js
@@ -0,0 +1,15 @@
+// const cart = [54, 56, 151, 235, 845, 12, 5, 100];
+// const cart1 = [213, 12, 4235, 656, 775, 89, 999]
+
+// const calculateTotalPrice = function (items) {
+//     let total = 0;
+
+//     for (const item of items) {
+//         total += item;
+//     }
+
+//     return total;
+// }
+
+// console.log(`Общая сумма покупок:`, calculateTotalPrice(cart));
+// console.log(`Общая сумма покупок:`, calculateTotalPrice(cart1));
