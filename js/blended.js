06876ffafb491e9658914eb84cbc059404eaa55b
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-08-27 14:11:29 +0300

    вапр

---
+++ js/blended.js
@@ -0,0 +1,52 @@
+// У нас есть объект, в котором храняться зарплаты команды.
+// Напишите код для сумирования всех зарплат и сохраните его результат в переменной sum.
+// Если объект salaries пуст, то результат должен быть 0.
+
+// const salaries = {
+//   Mango: 100,
+//   Poly: 160,
+//   Ajax: 1470,
+// };
+
+// const sum = Object.values(salaries).reduce((acc, salary) => acc + salary, 0)
+
+
+// console.log('Total Salary', sum)
+
+
+
+// Об'єднати два обєкти між собою:
+//
+// const users = [
+//   { name: "Victor", score: 20 },
+//   { name: "Mario", score: 10 },
+//   { name: "Tatiana", score: 30 },
+// ];
+
+// const moreUsers = [
+//   { name: "Laura", score: 50 },
+//   { name: "Jasmine", score: 70 },
+//   { name: "Peter", score: 60 },
+// ];
+
+// const allUsers = [...users, ...moreUsers]
+// console.log(allUsers)
+
+// Создайте объект calculator с тремя методами:
+// - read(a, b) - принимает два аргумента и сохраняет их как свойства объекта.
+// - sum() - возвращает сумму сохраненных значений.
+// - mult() - переумножает сохраненные значения и возвращает результат.
+
+// const calculator = {
+//     read(a, b) {this.a = a, this.b = b},
+//     sum() {return this.a + this.b},
+//     mult() {return this.a * this.b},
+// };
+// calculator.read(3, 5)
+// calculator.sum()
+// calculator.mult()
+// console.log(calculator)
+// console.log(calculator.sum())
+// console.log(calculator.mult())
+
+
--- js/vebinar.js
+++ js/vebinar.js
@@ -13,3 +13,61 @@
 
 // console.log(`Общая сумма покупок:`, calculateTotalPrice(cart));
 // console.log(`Общая сумма покупок:`, calculateTotalPrice(cart1));
+
+
+// Home Task
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    for (const potion of this.potions) {
+      if (potion.name === newPotion.name) {
+      return `Error! Potion ${newPotion} is already in your inventory!`;
+      }
+    }
+    this.potions.push(newPotion);
+  },
+  removePotion(potionName) {
+    let potionIndex = -1;
+    for (let i = 0; this.potions.length; i++){
+      if (this.potions[i].name === potionName) {
+        potionIndex = i;
+        break;
+      }
+    }
+
+    if (potionIndex === -1) {
+      return `Potion ${potionName} is not in inventory!`;
+    }
+
+    this.potions.splice(potionIndex, 1);
+  },
+  updatePotionName(oldName, newName) {
+    // const potionIndex = this.potions.indexOf(oldName);
+    // for (const potion of this.potions){
+    //    if (potion.name === oldName) {
+    //      this.potions.splice(potionIndex, 1, newName);
+    //    }
+    // }
+    
+    // if (potionIndex === -1) {
+    //   return `Potion ${oldName} is not in inventory!`;
+    // }
+    
+
+    this.potions.splice(this.potions.indexOf(oldName), 1, newName);
+  },
+  // Change code above this line
+};
+
+
+console.log(atTheOldToad.updatePotionName("Dragon breath", "Polymorth"))
+console.log(atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion"))
